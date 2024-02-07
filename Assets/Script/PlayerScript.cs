68d95bb4563abf3e5d0fe3f6d8c8803cf79b5e76
Lefebvre Gauthier <76998153+Iru713@users.noreply.github.com>
2024-02-07 16:32:24 +0100

    init

--- Assets/Script/PlayerScript.cs
+++ Assets/Script/PlayerScript.cs
@@ -2,8 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class PlayerScript : MonoBehaviour
 {
+    [SerializeField] private Rigidbody m_rb;
+    [SerializeField] private float m_speed;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,6 @@ public class PlayerScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        m_rb.velocity = new Vector3(Input.GetAxis("Horizontal") * m_speed, 0, Input.GetAxis("Vertical") * m_speed);
     }
 }
