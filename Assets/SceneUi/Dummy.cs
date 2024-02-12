69eb6f6df5ec853780512a7b9a0feeefef6f9000
brissou15 <brice.lambin@laposte.net>
2024-02-12 14:10:20 +0100

    Oui Dummy

---
+++ Assets/SceneUi/Dummy.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dummy : MonoBehaviour
+{
+
+    [SerializeField]
+    int HpMax;
+
+    int currentHeal;
+
+    bool dead = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHeal = HpMax;
+        GetComponent<Rigidbody>().isKinematic = true;
+
+    }
+
+    public void damagingDummy(int damage)
+    {
+        currentHeal -= damage;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void LateUpdate()
+    {
+        if(currentHeal <= 0 && !dead)
+        {
+            GetComponent<Rigidbody>().isKinematic = false;
+            GetComponent<Rigidbody>().AddForce(transform.forward, ForceMode.Impulse);
+            dead = true;
+        }
+
+
+        if(dead)
+        {
+            Destroy(gameObject, 3);
+        }
+    }
+}
