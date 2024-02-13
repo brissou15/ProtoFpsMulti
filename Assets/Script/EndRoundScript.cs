d27d75d6ffb6a55a23ea75f4696885c78d9c6674
Lefebvre Gauthier <76998153+Iru713@users.noreply.github.com>
2024-02-13 09:11:17 +0100

    input tir fini
    zone morte en serialize field et mise pour les déplacement et la caméra

---
+++ Assets/Script/EndRoundScript.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndRoundScript : MonoBehaviour
+{
+    [Header("Reference")]
+    [SerializeField] private GameObject panelHud;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
